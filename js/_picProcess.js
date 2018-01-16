427271fa8eb06e4ed8d5c6bb77e1f2d0b19ad35f
Kyle <308088483@qq.com>
2018-01-16 16:16:06 +0800

    Update _picProcess.js

--- js/_picProcess.js
+++ js/_picProcess.js
@@ -1,4 +1,4 @@
-var picProcess = {
+export var picProcess = {
         /**
          * [description]
          * @param  {[type]} e          [description]
@@ -6,34 +6,42 @@ var picProcess = {
          * @param  {[type]} oSselector [修改圖片選擇器]
          * @return {[type]}            [description]
          */
-        selectFileImage: function(e, nSelector, oSselector) {
-                var file = e.target.files[0],
-                        //拍攝方向
-                        orientation = null,
-                        fReader = new FileReader();
-                console.log('file:', file);
+        selectFileImage: function(e, settings, callback) {
+                var fReader = new FileReader(),
+                        formData = new FormData(),
+                        file = e.target.files[0],
+                        defaults = {
+                                minSize: 40 * 1024,
+                                maxSize: 10 * 1024 * 1024,
+                                reg: /image\/\w+/,
+                                width: 600,
+                                maxLen: null, //最大长度范围,会覆盖设定高宽
+                                encoderOptions: 0.8 //压缩质量
+                        },
+                        settings = picProcess.extend(defaults, settings),
+                        filtration = picProcess.filtration(file, settings);
+                // console.log('file:', file,filtration);
 
+                formData.append('file', file);
                 //限制大小格式
-                if (!picProcess.filtration(file)) {
+                if (!filtration) {
+                        return false;
+                } else if (Object.prototype.toString.call(filtration) === "[object Object]") {
+                        settings.callback && settings.callback({formData: formData})
                         return false;
                 }
-
-                //获取照片方向角属性，用户旋转控制
-                EXIF.getData(file, function() {
-                        orientation = EXIF.getTag(this, 'Orientation');
-                        console.log('旋转角度: ', orientation);
-                });
 
                 //轉碼完成
                 fReader.onload = function(e) {
                         //加載圖片
                         var img = new Image();
                         img.src = e.target.result;
-                        picProcess.preview(oSselector, e.target.result)
-                        img.onload = function() {
-                                // console.log('data: URL格式: ', this);
-                                var url = picProcess.changeDataURI.call(this, file.type, orientation);
-                                picProcess.preview(nSelector, url)
+                        img.onload = function(e) {
+                                var imgInfo = e.path[0],
+                                        nSize = picProcess.resetSize(this, imgInfo.width, imgInfo.height, settings),
+                                        dataURI = picProcess.changeDataURI.call(this, file.type, nSize.width, nSize.height, settings.encoderOptions);
+                                img.src = null;
+                                settings.callback && settings.callback({dataURI: dataURI})
                         }
                 }
 
@@ -47,16 +55,12 @@ var picProcess = {
          * @param  {[type]} settings [判斷條件]
          * @return {[type]}          [是否符合]
          */
-        filtration: function(file, extend) {
-                var extend = extend || {},
-                        settings = {
-                                size: extend.size || 5 * 1024 * 1024,
-                                reg: extend.reg || /image\/\w+/
-                        };
-
-                if (file.size > settings.size || !settings.reg.test(file.type)) {
-                        alert(extend.msg || ('上传格式非图片类型或上传图片超过' + settings.size / 1024 / 1024 + 'M'))
+        filtration: function(file, settings) {
+                if (file.size > settings.maxSize || !settings.reg.test(file.type)) {
+                        alert(settings.msg || ('上传格式非图片类型或上传图片超过' + settings.maxSize / 1024 / 1024 + 'M'))
                         return false;
+                } else if (file.size < settings.minSize) {
+                        return {};
                 }
                 return true;
         },
@@ -67,38 +71,15 @@ var picProcess = {
          * @param  {[type]} orientation [拍攝方向]
          * @return {[type]}             [DataURI]
          */
-        changeDataURI: function(type, orientation) {
+        changeDataURI: function(type, width, height, encoderOptions) {
                 var canvas = document.createElement('canvas'),
-                        ctx = canvas.getContext('2d'),
-                        nSize = picProcess.resetSize(this, 600, 400);
+                        ctx = canvas.getContext('2d');
 
-                switch (+ orientation) {
-                        case 3:
-                                canvas.width = nSize.width;
-                                canvas.height = nSize.height;
-                                ctx.rotate(180 * Math.PI / 180);
-                                ctx.drawImage(this, -nSize.width, -nSize.height, nSize.width, nSize.height);
-                                break;
-                        case 6:
-                                canvas.width = nSize.height;
-                                canvas.height = nSize.width;
-                                ctx.rotate(90 * Math.PI / 180);
-                                ctx.drawImage(this, 0, -nSize.height, nSize.width, nSize.height);
-                                break;
-                        case 8:
-                                canvas.width = nSize.height;
-                                canvas.height = nSize.width;
-                                ctx.rotate(270 * Math.PI / 180);
-                                ctx.drawImage(this, -nSize.width, 0, nSize.width, nSize.height);
-                                break;
-                        default:
-                                canvas.width = nSize.width;
-                                canvas.height = nSize.height;
-                                ctx.drawImage(this, 0, 0, nSize.width, nSize.height);
-                                break;
-                }
+                canvas.width = width;
+                canvas.height = height;
+                ctx.drawImage(this, 0, 0, width, height);
                 //图片展示的 data URI
-                return canvas.toDataURL(type, 0.8);
+                return canvas.toDataURL(type, encoderOptions);
         },
 
         /**
@@ -110,64 +91,41 @@ var picProcess = {
          * @param  {[type]} max    [最大範圍]
          * @return {[type]}        [寬高]
          */
-        resetSize: function(img, width, height, ratio, max) {
+        resetSize: function(img, width, height, settings) {
                 //前提是，必须在图片完全下载到客户端浏览器才能判断，
-                var w = img.naturalWidth,
-                        h = img.naturalHeight,
-                        width = width || w,
-                        height = height || h;
+                var resizeWidth = 0,
+                        resizeHeight = 0,
+                        max = settings.maxLen;
+                // console.log('原始寬高: ', width, height);
 
-                console.log('圖片寬高: ', w, h, '入參: ', width, height, ratio, max);
-
-                if (w != width)
-                        w = width;
-                if (h != height)
-                        h = height;
-                if (ratio && w / h != ratio)
-                        w = h * ratio;
-
-                //限制最大值範圍
-                if (max) {
-                        if (w > max && w >= h) {
-                                h = Math.ceil(h * max / w);
-                                w = max;
-                        } else if (h > max && h >= w) {
-                                w = Math.ceil(w * max / h);
-                                h = max;
+                if (!max) {
+                        resizeWidth = settings.width;
+                        resizeHeight = settings.height || Math.ceil(resizeWidth * height / width);
+                } else {
+                        //限制最大值範圍
+                        if (width > max && width >= height) {
+                                resizeWidth = max;
+                                resizeHeight = Math.ceil(resizeWidth * height / width);
+                        } else if (height > max && height >= width) {
+                                resizeHeight = max;
+                                resizeWidth = Math.ceil(width * resizeHeight / height);
                         }
                 }
-                console.log('重設寬高: ', w, h);
-                return {width: w, height: h}
+
+                // console.log('重設寬高: ', resizeWidth, resizeHeight);
+                return {width: resizeWidth, height: resizeHeight}
         },
 
         /**
-         * [預覽圖]
-         * @param  {[type]} selector [只簡單支持id和class]
-         * @param  {[type]} url      [圖片地址]
+         * [合并对象]
+         * @param  {[type]} defaults [被覆盖对象]
+         * @param  {[type]} source   [覆盖对象]
          * @return {[type]}          [description]
          */
-        preview: function(selector, url) {
-                var isId = /^#\w+/ig.test(selector),
-                        isClass = /^\.\w+/ig.test(selector),
-                        name = selector.slice(1),
-                        dom = null;
-
-                //選擇器類型
-                if (isId) {
-                        dom = document.getElementById(name);
-                } else if (isClass) {
-                        dom = document.getElementsByClassName(name)[0];
-                } else {
-                        alert('選擇器傳參不支持!');
-                        return false;
+        extend: function(defaults, source) {
+                for (var i in source) {
+                        defaults[i] = source[i]
                 }
-
-                //判斷類型
-                if (dom.nodeName == 'IMG') {
-                        dom.src = url;
-                } else {
-                        dom.style.backgroundImage = 'url(' + url + ')';
-                };
+                return defaults;
         }
-
 }
